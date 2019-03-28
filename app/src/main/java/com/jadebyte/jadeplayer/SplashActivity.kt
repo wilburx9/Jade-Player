3e69ca4fce891feb430a167d2b708b65891d07ec
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2019-03-28 01:43:24 +0100

    Added SplashActivity

---
+++ app/src/main/java/com/jadebyte/jadeplayer/SplashActivity.kt
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 . Wilberforce Uwadiegwu. All Rights Reserved.
+
+package com.jadebyte.jadeplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/jadebyte/jadeplayer/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 . Wilberforce Uwadiegwu. All Rights Reserved.
+
+package com.jadebyte.jadeplayer.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jadebyte.jadeplayer.R
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
