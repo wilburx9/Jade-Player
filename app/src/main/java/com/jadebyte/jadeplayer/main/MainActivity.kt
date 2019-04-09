204ad7336c3687d00e59e0e426a23a64e9be75ac
Wilberforce Uwadiegwu <wilburx9@gmail.com>
2019-04-09 07:02:50 +0100

    Added bottom navigation bar

--- app/src/main/java/com/jadebyte/jadeplayer/main/MainActivity.kt
+++ app/src/main/java/com/jadebyte/jadeplayer/main/MainActivity.kt
@@ -12,20 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+
         }
-        false
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
